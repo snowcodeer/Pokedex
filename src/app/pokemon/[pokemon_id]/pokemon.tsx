483fc6c9ad91291292357e90e938eeb50543aa03
Natalie <nataliechan1963@gmail.com>
2025-05-27 15:55:09 +0100

    visual edits, added evolution family

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,12 +1,11 @@
 import  Pokemon from "@/model/pokemon";
-import { Row, Col, Container, Image, ProgressBar} from 'react-bootstrap';
+import { Row, Col, Container, Image, ProgressBar, Badge} from 'react-bootstrap';
 import PokemonTypeBadgeComp from "@/components/pokemonTypeBadgeComp";
-
+import "./pokemonComponents.css";
 
 type Props ={
    pokemon: Pokemon;
 }
-
 
 export default function PokemonComponent(props : Props) {
    const {pokemon} = props;
@@ -31,18 +30,52 @@ export default function PokemonComponent(props : Props) {
                     <ProgressBar animated variant="warning" now={pokemon.defense} label={pokemon.defense} />
                     Speed
                     <ProgressBar animated variant="info" now={pokemon.speed} label={pokemon.speed} />
-                   
                     </div>
 
-                    <div className="card mt-4" style={{ width: '12rem' }}>
-                    <div className="card-body">
-                        <p className="card-title">Pokemon Type</p>
-                        <p className="card-text">
+                    <div className="card mt-4 w-100">
+                        <div className="card-body">
+                            <p className="card-title">Pokemon Type</p>
+                            <p className="card-text d-flex flex-wrap gap-2">
                             <PokemonTypeBadgeComp pokemonTypes={pokemon.pokemonType} />
-                        </p>
-                    </div>
-                    </div>
+                            </p>
+                        </div>
+                        </div>
 
+
+                    <div className="card mt-4 w-100">
+
+                    <div className="card-body">
+                        <p className="card-title">Evolution Family</p>
+                        <ul className="list-group list-group-flush">
+                        {pokemon.evolutionFamily.slice(0, 3).map((name: string, index: number) => {
+                            const currentIndex = pokemon.evolutionFamily.indexOf(pokemon.pokemonName);
+
+                            if (name === pokemon.pokemonName) {
+                            return (
+                                <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                                {name}
+                                <Badge bg="primary">Current</Badge>
+                                </li>
+                            );
+                            } else if (index > currentIndex) {
+                            return (
+                                <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                                {name}
+                                <Badge bg="success">Evolution</Badge>
+                                </li>
+                            );
+                            } else {
+                            return (
+                                <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                                {name}
+                                <Badge bg="danger">Devolution</Badge>
+                                </li>
+                            );
+                            }
+                        })}
+                        </ul>
+                    </div>
+                    </div>
 
                </Col>
                
