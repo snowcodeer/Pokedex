718032655d1cdede23a6bef9c15dd604d32a2f96
Natalie <nataliechan1963@gmail.com>
2025-05-27 13:46:03 +0100

    fixed card format

--- src/components/pokemonCardComp.tsx
+++ src/components/pokemonCardComp.tsx
@@ -1,51 +1,29 @@
 "use client";
 
+
 import PokemonCard from "@/model/pokemonCard";
 import { Card } from "react-bootstrap";
-import PokemonTypeBadgeComp from "./pokemonTypeBadgeComp";
+
 
 interface PokemonCardCompProps {
    pokemon: PokemonCard;
 }
 
+
 export default function PokemonCardComp(props: PokemonCardCompProps) {
+
+
    const pokemonUrl = `/pokemon/${props.pokemon.pokemonNumber}`;
 
+
    return (
-       <a href={pokemonUrl} style={styles.link}>
-           <Card style={styles.card}>
-               <Card.Img
-                   variant="top"
-                   src={props.pokemon.mainImage}
-                   alt={`${props.pokemon.pokemonName} image`}
-                   style={styles.image}
-               />
+       <a href={pokemonUrl}>
+           <Card>
+               <Card.Img variant="top" src={props.pokemon.mainImage} />
                <Card.Body>
                    <Card.Title>{props.pokemon.pokemonName}</Card.Title>
-                   <Card.Text>
-                       <PokemonTypeBadgeComp pokemonTypes={props.pokemon.pokemonType} />
-                   </Card.Text>
                </Card.Body>
            </Card>
        </a>
    );
 }
-
-const styles: { [key: string]: React.CSSProperties } = {
-   link: {
-     textDecoration: 'none',
-     color: 'inherit',
-     display: 'block',
-     width: '250px',
-   },
-   card: {
-     width: '100%',
-   },
-   image: {
-     width: '100%',
-     height: 'auto',           // Allow image to scale proportionally
-     maxHeight: '150px',       // Constrain maximum height
-     objectFit: 'contain',     // Ensure whole image is visible without cropping
-     backgroundColor: '#f8f8f8'// Optional: placeholder background
-   },
-};
