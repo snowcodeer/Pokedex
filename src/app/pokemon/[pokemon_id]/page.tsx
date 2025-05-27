f64b570e54c0b0cae171a7d819f91be3bbaceec1
Natalie <nataliechan1963@gmail.com>
2025-05-27 12:33:26 +0100

    edits

--- src/app/pokemon/[pokemon_id]/page.tsx
+++ src/app/pokemon/[pokemon_id]/page.tsx
@@ -1,54 +1,51 @@
 // File: /src/app/pokemon/[pokemon_id]/page.tsx
 
-'use client'
+'use client';
 import { Row, Container, Image, Spinner } from 'react-bootstrap';
 import Pokemon from '@/model/pokemon';
 import { useEffect, useState } from 'react';
 import PokemonComponent from './pokemon';
 import PokeNavBar from '@/components/pokeNavBarComp';
 
-// Extract pokemon_id from URL params
-type Params = { params: { pokemon_id: string } };
-
-export default function PokemonPage({ params }: Params) {
+export default function PokemonPage({ params }: { params: { pokemon_id: string } }) {
   const { pokemon_id } = params;
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  const [isPokemonLoaded, setPokemonLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
+    async function fetchData() {
       try {
-        const resp = await fetch('/api/pokemon/' + pokemon_id);
+        const resp = await fetch(`/api/pokemon/${pokemon_id}`);
         if (resp.ok) {
           const data: Pokemon = await resp.json();
           setPokemon(data);
         }
       } catch (error) {
-        console.error(error);
+        console.error('Fetch error:', error);
       } finally {
-        setPokemonLoaded(true);
+        setIsLoaded(true);
       }
-    };
+    }
     fetchData();
-  }, [pokemon_id]); // Add pokemon_id as dependency
+  }, [pokemon_id]);
 
   return (
     <>
       <PokeNavBar />
-      {isPokemonLoaded ? (
+      {isLoaded ? (
         pokemon ? (
           <PokemonComponent pokemon={pokemon} />
         ) : (
           <Image
             className="img-fluid mx-auto d-block rounded"
             src="https://cdn.dribbble.com/users/2805817/screenshots/13206178/media/6bd36939f8a01d4480cb1e08147e20f3.png"
-            alt="Pokémon not found placeholder"
+            alt="Pokémon not found"
           />
         )
       ) : (
         <Container>
           <Row className="justify-content-md-center p-2">
-            <Spinner className="p-2" animation="border" role="status" />
+            <Spinner animation="border" role="status" className="p-2" />
           </Row>
           <Row className="justify-content-md-center p-2">
             Loading Pokémon...
